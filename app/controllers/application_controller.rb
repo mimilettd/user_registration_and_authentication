5562b09868d5123421fbba71e376fec69ba97028
mimilettd <mimilettd@gmail.com>
2017-09-06 08:47:56 -0600

    limit authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  before_action :require_login
+
+  private
+
+  def require_login
+    unless current_user
+      flash[:error] = "You must be logged in to access this section"
+      redirect_to login_path
+    end
+  end
 end
